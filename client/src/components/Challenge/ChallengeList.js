78abd25d6d2e37692d941d57b943235146f2f539
CHOI-AHRIN <ahrin9610@naver.com>
2024-11-07 14:06:06 +0900

    ahrin: 첨부 및 랭킹파트 수정

--- client/src/components/Challenge/ChallengeList.js
+++ client/src/components/Challenge/ChallengeList.js
@@ -21,6 +21,7 @@ const ChallengeList = () => {
                 .catch(error => console.error('토큰에서 아이디를 읽어올 수 없습니다:', error));
         }
         fetchChallenges();
+        fetchMainChallenges();
     }, []);
 
     // 챌린지 목록 및 전체 페이지 수 설정
@@ -36,7 +37,21 @@ const ChallengeList = () => {
             .catch(error => console.error('챌린지 목록을 가져오는 중 오류:', error));
     };
 
-    // 현재 페이지에 맞는 챌린지 목록 슬라이스
+    // 공지사항 목록 및 전체 페이지 수 설정
+    const fetchMainChallenges = () => {
+        axios.get(`http://localhost:8080/api/challenge/challengeList?searchType=${searchtype}&keyword=${keyword}`)
+            .then(response => {
+                const fetchedChallenges = response.data.clist;
+                // 최신 순으로 정렬
+                const sortedChallenges = fetchedChallenges.sort((a, b) => new Date(b.wdate) - new Date(a.wdate));
+                setChallenges(sortedChallenges);
+                setTotalPages(Math.ceil(sortedChallenges.length / itemsPerPage));
+            })
+            .catch(error => console.error('공지사항 목록을 가져오는 중 오류:', error));
+    };
+
+
+    // 오늘의 챌린지
     const challengeListAppend = () => {
         const startIdx = (currentPage - 1) * itemsPerPage;
         const currentChallenges = challenges.slice(startIdx, startIdx + itemsPerPage);
@@ -62,6 +77,61 @@ const ChallengeList = () => {
             );
         });
     };
+    
+    // 챌린지 랭킹
+    const challengRankAppend = () => {
+        const startIdx = (currentPage - 1) * itemsPerPage;
+        const currentChallenges = challenges.slice(startIdx, startIdx + itemsPerPage);
+
+        return currentChallenges.map((data, index) => {
+            const date = data.wdate;
+            const year = date.substr(0, 4);
+            const month = date.substr(5, 2);
+            const day = date.substr(8, 2);
+            const reg_date = `${year}.${month}.${day}`;
+
+            // 현재 페이지와 항목 인덱스를 기반으로 순차적으로 번호를 표시
+            const num = startIdx + index + 1;
+
+            return (
+                <tr className="hidden_type" key={data.bno}>
+                    <td> {num} </td>
+                    <td> {data.mno}</td>
+                    <td> {data.uuid || '조회 중..'} </td>
+                    <td> {data.name} </td>
+                    <td> {reg_date} </td>
+                </tr>
+            );
+        });
+    };
+
+    // 공지사항
+   /*  const challengeMainAppend = () => {
+        const startIdx = (currentPage - 1) * itemsPerPage;
+        const currentChallenges = challenges.slice(startIdx, startIdx + itemsPerPage);
+
+        return currentChallenges.map((data, index) => {
+            const date = data.wdate;
+            const year = date.substr(0, 4);
+            const month = date.substr(5, 2);
+            const day = date.substr(8, 2);
+            const reg_date = `${year}.${month}.${day}`;
+
+            // 현재 페이지와 항목 인덱스를 기반으로 순차적으로 번호를 표시
+            const num = startIdx + index + 1;
+
+            return (
+                <tr className="hidden_type" key={data.bno}>
+                    <td> {num} </td>
+                    <td><Link to={`/ChallengeRead/${data.bno}`}>{data.title}{data.replycnt > 0 && `[${data.replycnt}]`}</Link></td>
+                    <td> {data.uuid || '조회 중..'} </td>
+                    <td> {data.bcounts} </td>
+                    <td> {reg_date} </td>
+                </tr>
+            );
+        });
+    }; */
+
 
     const handleSearchTypeChange = (e) => {
         setSearchtype(e.target.value);
@@ -115,6 +185,43 @@ const ChallengeList = () => {
         );
     };
 
+
+
+    // 공지사항 페이지네이션
+    /* const renderMainPagination = () => {
+        const pageNumbers = [];
+        const pagesToShow = 5; // 한 번에 보여줄 페이지 수
+
+        // 페이지네이션 표시 범위 계산
+        const startPage = Math.floor((currentPage - 1) / pagesToShow) * pagesToShow + 1;
+        const endPage = Math.min(startPage + pagesToShow - 1, totalPages);
+
+        for (let i = startPage; i <= endPage; i++) {
+            const isCurrentPage = i === currentPage;
+            pageNumbers.push(
+                <button style={{ margin: 5, backgroundColor: isCurrentPage ? '#20217d' : '' }}
+                    className={`sch_bt99 wi_au ${isCurrentPage ? 'current-page' : ''}`} key={i} onClick={() => handlePageClick(i)}>
+                    {i}
+                </button>
+            );
+        };
+        return (
+            <div className="Paging">
+                {totalPages > pagesToShow && startPage > 1 && (
+                    <button style={{ margin: 5 }} className="sch_bt99 wi_au" onClick={() => handlePageClick(startPage - 1)}>
+                        {'<'}
+                    </button>
+                )}
+                {pageNumbers}
+                {totalPages > pagesToShow && endPage < totalPages && (
+                    <button style={{ margin: 5 }} className="sch_bt99 wi_au" onClick={() => handlePageClick(endPage + 1)}>
+                        {'>'}
+                    </button>
+                )}
+            </div>
+        );
+    };
+ */
     return (
         <section className="sub_wrap">
             <article className="s_cnt mp_pro_li ct1 mp_pro_li_admin">
@@ -122,6 +229,36 @@ const ChallengeList = () => {
                     <h2 className="s_tit1">챌린지</h2>
                 </div>
 
+                {/* 챌린지 공지사항 */}
+               {/*  <h4 className="s_tit2">공지사항</h4>
+                <div className="list_cont list_cont_admin">
+                    <table className="table_ty1 ad_tlist">
+                        <tr>
+                            <th>번호</th>
+                            <th>제목</th>
+                            <th>작성자</th>
+                            <th>조회수</th>
+                            <th>작성일</th>
+                        </tr>
+                    </table>
+                    <table id="appendChallengeList" className="table_ty2 ad_tlist">
+                        {challengeMainAppend()}
+                    </table>
+                    <div id="spaging">
+                        {renderMainPagination()}
+                    </div>
+                </div>
+
+                {currentUuid === 'admin' && (
+                    <div className="li_top_sch af">
+                        <Link to={'/ChallengeInsert'} className="sch_bt2 wi_au">글쓰기</Link>
+                    </div>
+                )} */}
+
+                {/* --------------------------------------------------------------------------------------------------------------------- */}
+
+
+                <h4 className="s_tit2">오늘의 챌린지</h4>
                 <div className="searchingForm">
                     <form onSubmit={(e) => handleSearchButtonClick(e)}>
                         <select value={searchtype} onChange={handleSearchTypeChange} id="searchtype" className="searchzone">
@@ -159,6 +296,26 @@ const ChallengeList = () => {
                         <Link to={'/ChallengeInsert'} className="sch_bt2 wi_au">글쓰기</Link>
                     </div>
                 )}
+
+                {/* --------------------------------------------------------------------------------------------------------------------- */}
+
+                {/* 챌린지 랭킹 */}
+                <h4 className="s_tit2">챌린지 랭킹</h4>
+
+                <div className="list_cont">
+                    <table className="table_ty1 ad_tlist">
+                        <tr>
+                            <th>순위</th>
+                            <th>회원번호</th>
+                            <th>아이디</th>
+                            <th>이름</th>
+                            <th>인증수</th>
+                        </tr>
+                    </table>
+                    <table id="appendChallengeList" className="table_ty2 ad_tlist">
+                        {challengeListAppend()}
+                    </table>
+                </div>
             </article>
         </section>
     );
